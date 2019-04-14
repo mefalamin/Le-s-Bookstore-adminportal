460d11a5944481e016b9477f51852f3d8dd4903f
Al-Amin Ebne Faiz <mefalamin@live.com>
2019-04-15 02:01:45 +0600

    updateBook logic added in the adminportal

--- src/main/java/com/adminportal/controller/BookController.java
+++ src/main/java/com/adminportal/controller/BookController.java
@@ -11,10 +11,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.websocket.server.PathParam;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 /**
@@ -77,6 +78,44 @@ public class BookController {
         return "bookInfo";
     }
 
+    @RequestMapping(value = "/updateBook" ,method = RequestMethod.GET)
+    public String updateBook(
+            @RequestParam("id") Long id ,
+            Model model
+    )
+    {
+        Book book = bookService.findOne(id);
+        model.addAttribute("book",book);
+        return "updateBook";
+    }
+
+    @RequestMapping(value = "/updateBook",method = RequestMethod.POST)
+    public String updateBookPost(
+            @ModelAttribute("book") Book book,
+            Model model
+    )
+    {
+
+        bookService.save(book);
+        MultipartFile bookImage = book.getBookImage();
+
+        if(!bookImage.isEmpty()){
+            String name = book.getId()+".png";
+            try{
+                byte[] bytes = bookImage.getBytes();
+                Files.delete(Paths.get("src/main/resources/static/images/book/"+name));
+                BufferedOutputStream stream = new BufferedOutputStream(
+                        new FileOutputStream(new File("src/main/resources/static/images/book/"+name)));
+                stream.write(bytes);
+                stream.close();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        model.addAttribute("book",book);
+        return "redirect:bookInfo?id="+book.getId();
+    }
 
 
 }
